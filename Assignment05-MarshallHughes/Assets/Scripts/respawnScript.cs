f79130074ae17a37f53a68b7713d173ab073af64
Koinu <bathavenue@hotmail.com>
2020-10-08 16:47:53 -0400

    Assignment05-MarshallHughes
    test

---
+++ Assignment05-MarshallHughes/Assets/Scripts/respawnScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawnScript : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public GameObject respawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                prefab.transform.position = respawn.transform.position;
+            }
+
+        }
+
+       
+    }
+}
